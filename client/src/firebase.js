cca587cbc39935563ea3835240ab361bbb2fe8c2
Sunnythakur847583 <sunnykumarthakur29907@gmail.com>
2025-01-06 20:55:15 +0530

    mern-estate

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,22 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  apiKey: "AIzaSyAyJXxw4a0TQAkv7TQv7Ilw69s1uMrzLjs",
+  authDomain: "mern-estate-20bf6.firebaseapp.com",
+  projectId: "mern-estate-20bf6",
+  storageBucket: "mern-estate-20bf6.appspot.com", // Fixed typo here (if incorrect URL was provided).
+  messagingSenderId: "284121733678",
+  appId: "1:284121733678:web:9578ec1e06cf348b929dbf",
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig); // No `export` here
+const storage = getStorage(app);
+
+// Export `app` and `storage` in a single statement
+export { app, storage };
